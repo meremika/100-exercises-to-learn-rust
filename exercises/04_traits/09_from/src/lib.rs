81e74fb9adf3a64263a634846de740cf5a584102
Michael Kaiser <michael.kaiser@gfn.education>
2025-06-30 14:19:10 +0200

    04_traits/09_from

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -1,10 +1,17 @@
+#![allow(dead_code)]
 // TODO: Implement the `From` trait for the `WrappingU32` type to make `example` compile.
 
 pub struct WrappingU32 {
     value: u32,
 }
 
+impl From<u32> for WrappingU32 {
+    fn from(value: u32) -> Self {
+        Self { value }
+    }
+}
+
 fn example() {
-    let wrapping: WrappingU32 = 42.into();
-    let wrapping = WrappingU32::from(42);
+    let _wrapping: WrappingU32 = 42.into();
+    let _wrapping = WrappingU32::from(42);
 }
