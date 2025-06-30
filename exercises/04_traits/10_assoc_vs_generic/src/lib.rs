da2cd365e359e1ca54e563676b0b68e30ddfe673
Michael Kaiser <michael.kaiser@gfn.education>
2025-06-30 14:25:12 +0200

    04_traits/10_assoc_vs_generic

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -13,6 +13,36 @@
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
 
+pub trait Power<T> {
+    type Output;
+
+    fn power(&self, other: T) -> Self::Output;
+}
+
+impl Power<u16> for u32 {
+    type Output = u32;
+
+    fn power(&self, other: u16) -> Self::Output {
+        self.pow(other as u32)
+    }
+}
+
+impl Power<u32> for u32 {
+    type Output = u32;
+
+    fn power(&self, other: u32) -> Self::Output {
+        self.pow(other)
+    }
+}
+
+impl Power<&u32> for u32 {
+    type Output = u32;
+
+    fn power(&self, other: &u32) -> Self::Output {
+        self.pow(*other)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Power;
