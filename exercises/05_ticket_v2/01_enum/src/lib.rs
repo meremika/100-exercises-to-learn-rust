cf2fcdc7fac9d9c581a6485eebb90e6d97789d47
Michael Kaiser <michael.kaiser@gfn.education>
2025-07-01 09:02:14 +0200

    05_ticket_v2/01_enum

--- exercises/05_ticket_v2/01_enum/src/lib.rs
+++ exercises/05_ticket_v2/01_enum/src/lib.rs
@@ -4,18 +4,21 @@
 #[derive(Debug, PartialEq)]
 // `derive`s are recursive: it can only derive `PartialEq` if all fields also implement `PartialEq`.
 // Same holds for `Debug`. Do what you must with `Status` to make this work.
-struct Ticket {
+pub struct Ticket {
     title: String,
     description: String,
-    status: String,
+    status: Status,
 }
 
-enum Status {
-    // TODO: add the missing variants
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Status {
+    ToDo,
+    InProgress,
+    Done,
 }
 
 impl Ticket {
-    pub fn new(title: String, description: String, status: String) -> Ticket {
+    pub fn new(title: String, description: String, status: Status) -> Ticket {
         if title.is_empty() {
             panic!("Title cannot be empty");
         }
@@ -27,9 +30,6 @@ impl Ticket {
         }
         if description.len() > 500 {
             panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
         }
 
         Ticket {
@@ -47,7 +47,7 @@ impl Ticket {
         &self.description
     }
 
-    pub fn status(&self) -> &String {
+    pub fn status(&self) -> &Status {
         &self.status
     }
 }
