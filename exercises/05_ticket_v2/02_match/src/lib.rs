84ba789be923c225ecc342740403f343101f6cdb
Michael Kaiser <michael.kaiser@gfn.education>
2025-07-01 09:11:25 +0200

    05_ticket_v2/02_match

--- exercises/05_ticket_v2/02_match/src/lib.rs
+++ exercises/05_ticket_v2/02_match/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 enum Shape {
     Circle,
     Square,
@@ -9,7 +10,12 @@ enum Shape {
 impl Shape {
     // TODO: Implement the `n_sides` method using a `match`.
     pub fn n_sides(&self) -> u8 {
-        todo!()
+        match self {
+            Shape::Circle => 0,
+            Shape::Triangle => 3,
+            Shape::Square | Shape::Rectangle => 4,
+            Shape::Pentagon => 5,
+        }
     }
 }
 
