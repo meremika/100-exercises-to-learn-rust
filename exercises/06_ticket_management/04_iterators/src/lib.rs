3e33a07b95450d51bfea7f7d494eaffa00d0bbc1
Michael Kaiser <michael.kaiser@gfn.education>
2025-07-01 12:39:24 +0200

    06_ticket_management/04_iterators

--- exercises/06_ticket_management/04_iterators/src/lib.rs
+++ exercises/06_ticket_management/04_iterators/src/lib.rs
@@ -13,6 +13,16 @@ pub struct TicketStore {
     tickets: Vec<Ticket>,
 }
 
+impl IntoIterator for TicketStore {
+    type Item = Ticket;
+
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.tickets.into_iter()
+    }
+}
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct Ticket {
     pub title: TicketTitle,
@@ -28,6 +38,7 @@ pub enum Status {
 }
 
 impl TicketStore {
+    #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
         Self {
             tickets: Vec::new(),
