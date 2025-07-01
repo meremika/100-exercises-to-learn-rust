0ecc7be7bea7d4212d9bd93c9dfac8c917276026
Michael Kaiser <michael.kaiser@gfn.education>
2025-07-01 12:59:47 +0200

    06_ticket_management/07_combinators

--- exercises/06_ticket_management/07_combinators/src/lib.rs
+++ exercises/06_ticket_management/07_combinators/src/lib.rs
@@ -22,6 +22,7 @@ pub enum Status {
 }
 
 impl TicketStore {
+    #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
         Self {
             tickets: Vec::new(),
@@ -30,6 +31,13 @@ impl TicketStore {
 
     pub fn add_ticket(&mut self, ticket: Ticket) {
         self.tickets.push(ticket);
+    }
+
+    pub fn to_dos(&self) -> Vec<&Ticket> {
+        self.tickets
+            .iter()
+            .filter(|t| t.status == Status::ToDo)
+            .collect()
     }
 }
 
