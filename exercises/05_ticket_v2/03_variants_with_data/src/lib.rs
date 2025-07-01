3e31c2b1502b112b9378e1efed9067e618931752
Michael Kaiser <michael.kaiser@gfn.education>
2025-07-01 09:17:30 +0200

    05_ticket_v2/03_variants_with_data

--- exercises/05_ticket_v2/03_variants_with_data/src/lib.rs
+++ exercises/05_ticket_v2/03_variants_with_data/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 // TODO: Implement `Ticket::assigned_to`.
 //  Return the name of the person assigned to the ticket, if the ticket is in progress.
 //  Panic otherwise.
@@ -38,7 +39,10 @@ impl Ticket {
         }
     }
     pub fn assigned_to(&self) -> &str {
-        todo!()
+        match &self.status {
+            Status::InProgress { assigned_to } => assigned_to,
+            _ => panic!("Only `In-Progress` tickets can be assigned to someone"),
+        }
     }
 }
 
