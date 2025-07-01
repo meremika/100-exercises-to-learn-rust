464003d7efc2fd789fc2678ec555ababe74133c0
Michael Kaiser <michael.kaiser@gfn.education>
2025-07-01 12:28:36 +0200

    06_ticket_management/02_vec

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,12 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let n = n as usize;
+    let mut values = vec![0, 1];
+    for i in 2..=n {
+        values.push(values[i - 2] + values[i - 1])
+    }
+    values[n]
 }
 
 #[cfg(test)]
