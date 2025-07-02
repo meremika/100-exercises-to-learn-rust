cd7e044b2ced97c7498158718ceac982ac7869c0
Michael Kaiser <michael.kaiser@gfn.education>
2025-07-02 12:50:19 +0200

    08_futures/04_future

--- exercises/08_futures/04_future/src/lib.rs
+++ exercises/08_futures/04_future/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 //! TODO: get the code to compile by **re-ordering** the statements
 //!  in the `example` function. You're not allowed to change the
 //!  `spawner` function nor what each line does in `example`.
@@ -10,7 +11,9 @@ fn spawner() {
 }
 
 async fn example() {
-    let non_send = Rc::new(1);
+    {
+        let non_send = Rc::new(1);
+        println!("{non_send}");
+    }
     yield_now().await;
-    println!("{}", non_send);
 }
